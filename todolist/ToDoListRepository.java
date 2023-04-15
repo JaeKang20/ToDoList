99e95afa7c7651c50aba059aaf82c133974a2fd1
최재강 <jkchoi7091@knou.ac.kr>
2023-04-16 00:00:39 +0900

    Add files via upload
    todolist with kakao login

---
+++ todolist/ToDoListRepository.java
@@ -0,0 +1,9 @@
+package com.myself.todolist;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToDoListRepository extends JpaRepository<ToDoListEntity, Long> {
+	
+}
+
